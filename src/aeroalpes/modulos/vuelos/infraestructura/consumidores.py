f8ca3a5ff798b993f30e577400f610fed97afb07
walarconc alarcon <w.alarconc@uniandes.edu.co>
2025-02-27 20:28:13 +0000

    Ajuste

--- src/aeroalpes/modulos/vuelos/infraestructura/consumidores.py
+++ src/aeroalpes/modulos/vuelos/infraestructura/consumidores.py
@@ -6,8 +6,8 @@ import logging
 import traceback
 import datetime
 
-from aeroalpes.modulos.auditoria.infraestructura.schema.v1.eventos import EventoRegulacionCreada
-from aeroalpes.modulos.auditoria.infraestructura.schema.v1.comandos import ComandoCrearRegulacion
+from aeroalpes.modulos.vuelos.infraestructura.schema.v1.eventos import EventoReservaCreada
+from aeroalpes.modulos.vuelos.infraestructura.schema.v1.comandos import ComandoCrearReserva
 
 
 from aeroalpes.modulos.vuelos.infraestructura.proyecciones import ProyeccionReservasLista, ProyeccionReservasTotales
@@ -15,16 +15,15 @@ from aeroalpes.seedwork.infraestructura.proyecciones import ejecutar_proyeccion
 from aeroalpes.seedwork.infraestructura import utils
 
 def suscribirse_a_eventos(app=None):
-    print("SUSCRBIRSE A ESUCHAR LOS EVENTOS DE PULSAR AL INICAR LA APP REGULACIONES")
     cliente = None
     try:
         cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        consumidor = cliente.subscribe('eventos-reserva', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='aeroalpes-sub-eventos', schema=AvroSchema(EventoRegulacionCreada))
+        consumidor = cliente.subscribe('eventos-reserva', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='aeroalpes-sub-eventos', schema=AvroSchema(EventoReservaCreada))
 
         while True:
             mensaje = consumidor.receive()
             datos = mensaje.value().data
-            print(f'Evento recibido EN PULSAR: {datos}')
+            print(f'Evento recibido: {datos}')
 
             # TODO Identificar el tipo de CRUD del evento: Creacion, actualización o eliminación.
             ejecutar_proyeccion(ProyeccionReservasTotales(datos.fecha_creacion, ProyeccionReservasTotales.ADD), app=app)
@@ -43,7 +42,7 @@ def suscribirse_a_comandos(app=None):
     cliente = None
     try:
         cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        consumidor = cliente.subscribe('comandos-reserva', consumer_type=_pulsar.ConsumerType.Shared, subscription_name='aeroalpes-sub-comandos', schema=AvroSchema(ComandoCrearRegulacion))
+        consumidor = cliente.subscribe('comandos-reserva', consumer_type=_pulsar.ConsumerType.Shared, subscription_name='aeroalpes-sub-comandos', schema=AvroSchema(ComandoCrearReserva))
 
         while True:
             mensaje = consumidor.receive()
